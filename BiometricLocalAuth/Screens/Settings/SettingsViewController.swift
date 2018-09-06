7546bad108c5578174377983bf8f9a6edcb30dda
Eugene Lezov <el@appkode.ru>
2018-09-06 18:31:35 +0200

    add switch for penguin animation

--- BiometricLocalAuth/Screens/Settings/SettingsViewController.swift
+++ BiometricLocalAuth/Screens/Settings/SettingsViewController.swift
@@ -16,6 +16,7 @@ final class SettingsViewController: UIViewController, SettingsView {
     var isBack: Bool = false
     var onFinish: (() -> Void)?
     var animationView: LOTAnimationView!
+    var toggle: LOTAnimatedSwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +26,7 @@ final class SettingsViewController: UIViewController, SettingsView {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        animationView.play()
+        if toggle.isOn { animationView.play() }
         if isBack {
             let backBarButton = UIBarButtonItem(barButtonSystemItem: .undo,
                                                 target: self,
@@ -39,18 +40,49 @@ final class SettingsViewController: UIViewController, SettingsView {
         animationView.stop()
     }
     
-    private func configureAppereance() {
+    private func configurePenguin() {
         animationView = LOTAnimationView(name: "techno_penguin")
         animationView.contentMode = .scaleAspectFill
         animationView.loopAnimation = true
         self.view.addSubview(animationView)
         animationView.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
+            make.center.equalToSuperview().offset(100)
+            make.right.equalToSuperview().offset(-64)
+            make.left.equalToSuperview().offset(64)
+            make.height.equalTo(300)
+        }
+        animationView.play()
+    }
+    
+    private func configureAppereance() {
+        toggle = LOTAnimatedSwitch(named: "Switch_States")
+        toggle.setOn(false, animated: false)
+        toggle.setProgressRangeForOnState(fromProgress: 1, toProgress: 0)
+        toggle.setProgressRangeForOffState(fromProgress: 0, toProgress: 1)
+        
+        toggle.addTarget(self, action: #selector(switchTapped), for: .valueChanged)
+        view.addSubview(toggle)
+        
+        toggle.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.height.equalTo(70)
+            make.width.equalTo(140)
         }
     }
     
     @objc
     private func backButtonTapped() {
         onFinish?()
+    }
+    
+    @objc
+    private func switchTapped() {
+        if toggle.isOn {
+            configurePenguin()
+        } else {
+            animationView.stop()
+            animationView.removeFromSuperview()
+        }
     }
 }
